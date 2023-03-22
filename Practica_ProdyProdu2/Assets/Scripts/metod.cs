952c23aff0595b194fa4e6f3abf6b38848aa1939
daniel13524 <danielenrique.saves@hotmail.com>
2023-03-22 17:42:17 -0500

    metodo

---
+++ Practica_ProdyProdu2/Assets/Scripts/metod.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class metod : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        startDialogue();
+    }
+
+    public void startDialogue()
+    {
+        Debug.Log("NPC Says : Hello");
+    }
+}
