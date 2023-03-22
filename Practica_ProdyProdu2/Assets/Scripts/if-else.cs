2e530736963a47a58745907c51b2834652cc4ebc
Jose Carlos Leiva <josecarlosleivi@gmail.com>
2023-03-21 20:36:54 -0500

    if-else

---
+++ Practica_ProdyProdu2/Assets/Scripts/if-else.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parametro : MonoBehaviour
+{
+    public interface myNumber= 10;
+
+    void Start()
+    {
+        if (myNumber == 10)
+        {
+            print("myNumber is equal to 10");
+        }
+        else if (myNumber == 15)
+        {
+            print("myNumber is equal to 15");
+        }
+        else
+        {
+            print("myNumber is not equal to 10");
+        }
+    }
+}
+
+
