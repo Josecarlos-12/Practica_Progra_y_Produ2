a26044e78149cbbfcd9fb3f940075e35f1cdb778
Jose Carlos Leiva <josecarlosleivi@gmail.com>
2023-03-21 20:27:30 -0500

    Parametro

---
+++ Practica_ProdyProdu2/Assets/Scripts/Parametro.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parametro : MonoBehaviour
+{
+    public void Suma(int 1, int 2)
+    {
+        print("Suma:" + 1 + 2);
+    }
+    
+}
+
+
